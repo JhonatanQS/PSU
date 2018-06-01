c6e702c2d79c1abac1840ac7caf68aff76d9f983
Jhonatan Queiroz <jhonatanqsilva@gmail.com>
2018-05-31 22:10:24 -0300

    Arquivos Iniciais

---
+++ src/app/oferta/oferta.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+
+@Component({
+  selector: 'app-oferta',
+  templateUrl: './oferta.component.html',
+  styleUrls: ['./oferta.component.css']
+})
+export class OfertaComponent implements OnInit {
+
+  constructor(private route: ActivatedRoute) {
+
+   }
+
+  ngOnInit() {
+    this.route.snapshot.params['id']
+  }
+
+}
